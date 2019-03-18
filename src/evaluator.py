5029b1d6bcf58629b7003f7f711b589c90162576
benjamin@helios0 <abruzzo2@gmail.com>
2019-03-18 15:22:24 -0400

    creates sub-images form overlappting class bounding boxes

--- src/evaluator.py
+++ src/evaluator.py
@@ -5,7 +5,14 @@ import math
 import sys
 import rospy
 import cv2
+
+from usma_threat_ros.msg import ImageArray
 from darknet_ros_msgs.msg import BoundingBox, BoundingBoxes
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+
+
 import time
 import tf
 
@@ -19,13 +26,32 @@ class box_evaluator():
         self.msg_seq = 0
         self.msg_time = 0
         self.human_type = "person"
-        self.gun_type = "backpack"
+        self.gun_type = "tvmonitor"
 
         self.gun_boxes = []
         self.human_boxes = []
+        self.pose_imgs = []
+        self.potential_threats = []
+        self.potential_threats_imgs = ImageArray()
+
+        self.bridge = CvBridge()
+        self.srcimg_topic = rospy.get_param("~srcimg_topic","/ardrone/front/image_rect_color")
+        self.srcimg_sub = rospy.Subscriber(self.srcimg_topic,Image,self.updateimage)
+
+        # self.threatbox_pub = rospy.Publisher("threats",Image)
+
+
+
+        # /ardrone/front/image_rect_color
 
         # self.bboxes = {self.human_type:[], self.gun_type:[]}
 
+    def updateimage(self,msg):
+        try:
+            self.cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            self.image_rx = True
+        except CvBridgeError as e:
+            print(e)
 
     def yolo_boxes(self,msg):
         if (self.msg_time != msg.header.stamp.to_sec()):
@@ -38,6 +64,7 @@ class box_evaluator():
             # self.bboxes = {self.human_type:[], self.gun_type:[]}
             self.gun_boxes = []
             self.human_boxes = []
+            self.potential_threats = []
 
             for box in self.yolo_box_data:
                 if box.Class == self.human_type:
@@ -46,18 +73,26 @@ class box_evaluator():
                 if box.Class == self.gun_type:
                     self.gun_boxes.append({'xmin':box.xmin,'ymin':box.ymin,'xmax':box.xmax,'ymax':box.ymax})
 
-
             # Now loop over bboxes to see if any guns and people overlap
         for person_box in self.human_boxes:
             for gun_box in self.gun_boxes:
-                print("\n")
-                print("person_box, xmin:[{}], xmax:[{}], ymin:[{}], ymax:[{}] ").format(person_box["xmin"], person_box["xmax"], person_box["ymin"], person_box["ymax"]) 
-                print("gun_box, xmin:[{}], xmax:[{}], ymin:[{}], ymax:[{}] ").format(gun_box["xmin"], gun_box["xmax"], gun_box["ymin"], gun_box["ymax"]) 
-                print("Person and Gun Overlap: {}").format(self.box_overlap(person_box, gun_box))
+                # print("\n")
+                # print("person_box, xmin:[{}], xmax:[{}], ymin:[{}], ymax:[{}] ").format(person_box["xmin"], person_box["xmax"], person_box["ymin"], person_box["ymax"]) 
+                # print("gun_box, xmin:[{}], xmax:[{}], ymin:[{}], ymax:[{}] ").format(gun_box["xmin"], gun_box["xmax"], gun_box["ymin"], gun_box["ymax"]) 
+                if self.box_overlap(person_box, gun_box):
+                    self.potential_threats.append(person_box)
                 # to get here, there must be both a gun and a person present in the frame
+        
+        if self.image_rx:
+            self.potential_threats_imgs = ImageArray()
+            self.potential_threats_imgs.header = msg.header
 
-
-        # if (human min < (gun min or gun max)) or (human max > )
+            i = 0
+            for person_box in self.potential_threats:
+                print("person_box[{}], xmin:[{}], xmax:[{}], ymin:[{}], ymax:[{}] ").format(i, person_box["xmin"], person_box["xmax"], person_box["ymin"], person_box["ymax"]) 
+                crop_img = self.cv_image[person_box["ymin"]:person_box["ymax"],  person_box["xmin"]:person_box["xmax"]]
+                self.potential_threats_imgs.images.append(self.bridge.cv2_to_imgmsg(crop_img, "bgr8"))
+                i += 1
 
     def box_overlap(self, human_box, gun_box):
         # Overlapping rectangles overlap both horizontally & vertically
