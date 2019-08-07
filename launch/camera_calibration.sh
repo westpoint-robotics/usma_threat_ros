4b62325d059f655ad175292631b639f7db935bec
benjamin@titan <abruzzo2@gmail.com>
2019-08-07 15:19:38 -0400

    added calibration tweaks

---
+++ launch/camera_calibration.sh
@@ -0,0 +1,3 @@
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.107 image:=/pgr_1/image_raw camera:=/pgr_1
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.107 image:=/pgr_2/image_raw camera:=/pgr_2
+rosrun camera_calibration cameracalibrator.py --size 8x6 --square 0.107 image:=/ardrone/front/image_raw camera:=/ardrone/front
